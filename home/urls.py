9c8aa002e7e006547cac5891fc0de00361857f72
Miryabbelli Tarun <miryabbellitarun@gmail.com>
2026-05-18 18:56:26 +0530

    vehicle details page added

--- home/urls.py
+++ home/urls.py
@@ -5,5 +5,6 @@ from home import views
 urlpatterns = [    
     path('',views.home,name='home'),
     path('category-list/',views.category_list_view,name='category_list'),
+    path('vehicle_details/<slug:vehicle_slug>/',views.vehicle_details_view,name='vehicle_details'),
 
 ]
--- home/views.py
+++ home/views.py
@@ -1,15 +1,17 @@
-from django.shortcuts import render
+from django.shortcuts import get_object_or_404, render
 
-from home.models import Banner, Category
+from home.models import Banner, Category, Vehicle
 
 # Create your views here.
 def home(request):
     categories=Category.objects.all().order_by('?')[:6]
     banners=Banner.objects.filter(is_active=True)
+    vehicles=Vehicle.objects.filter(availability=True)
     
     context={
         "categories":categories,
-        'banners':banners
+        'banners':banners,
+        'vehicles':vehicles
     }
     return render(request,'home.html',context)
 
@@ -19,3 +21,12 @@ def category_list_view(request):
         'categories':categories
     }
     return render(request,'category_list.html',context)
+
+
+def vehicle_details_view(request,vehicle_slug):
+    vehicle=get_object_or_404(Vehicle,slug=vehicle_slug)
+    # print(vehicle)
+    context={
+        'vehicle':vehicle,
+    }
+    return render(request,'vehicle_details.html',context)
